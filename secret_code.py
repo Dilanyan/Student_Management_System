9b733e6c8297b09fa84850b2a20adc1d8b670d94
Dilanyan <dilanyan.n@gmail.com>
2025-07-02 22:08:58 +0400

    Improvement of secret code 4

--- secret_code.py
+++ secret_code.py
@@ -1,8 +1,20 @@
 secret_name = input("Hi, please, enter your name (You are the person who will say the secret number): ")
 guess_name = input("Hi, please, enter your name (You are the person who will guess the secret number): ")
 
-secret_number = int(input("Input secret number "))
-guess_number = int(input("Input guess number "))
+secret_number = input("Input secret number ")
+while not secret_number.isdigit():
+    print("Sorry, ---{}--- is not a number. Try again, please".format(secret_number))
+    secret_number = input("Input secret number ")
+
+secret_number = int(secret_number)
+
+guess_number = input("Input guess number ")
+while not guess_number.isdigit():
+    print("Sorry, ---{}--- is not a number. Try again, please".format(guess_number))
+    guess_number = input("Input guess number ")
+
+guess_number = int(guess_number)
+
 
 attempts = 1
 too_high = secret_number + 10
@@ -21,5 +33,9 @@ while 1 > 0:
     else:
         print("{}, you are soooo close, try again!".format(guess_name))
 
-    guess_number = int(input("Input guess number "))
+    guess_number = input("Input guess number ")
+    while guess_number.isdigit() == False:
+        print("Sorry, ---{}--- is not a number. Try again, please".format(guess_number))
+        guess_number = input("Input guess number ")
 
+    guess_number = int(guess_number)
