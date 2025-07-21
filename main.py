c653fcf30fddf310314b5609f8e9048b94f55ee5
Dilanyan <dilanyan.n@gmail.com>
2025-07-21 20:06:18 +0400

    A little changes

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import functions
 
 input_max_student_number = input("Input the maximum number of students: ")
-# while not input_max_student_number.isdigit() or input_max_student_number == "0":
-#     print(f"Please, input a number")
-#     input_max_student_number = input("Input the maximum number of students: ")
-#
-# max_student_number = int(input_max_student_number)
 max_student_number = functions.input_digit_from_user(input_max_student_number, "Input the maximum number of students")
 
-students = []
-students1 = dict()
+students = dict()
 student_counts = 0
 input_student_or_not = ""
 
@@ -21,12 +15,7 @@ while student_counts < max_student_number:
     surname = input("Enter student surname: ")
     user_surname = functions.input_username_surename(surname, "Enter student surname")
 
-
     student_age = input("Enter student age (from 18 to 120): ")
-
-    # while not student_age.isdigit():
-    #     student_age = input("Enter student age (from 18 to 120): ")
-    # student_age = int(student_age)
     student_age = functions.input_digit_from_user(student_age, "Enter student age (from 18 to 120)")
 
 
@@ -37,32 +26,21 @@ while student_counts < max_student_number:
         print("Now let's to calculate the average grade for previous and current year - ")
 
         currentYearGrade = input("Enter student current year grade. From 1 to 100: ")
-        while not currentYearGrade.isdigit() or currentYearGrade == "0":
-            currentYearGrade = input("Enter student current year grade. From 1 to 100: ")
-
-        currentYearGrade = int(currentYearGrade)
+        currentYearGrade = functions.input_digit_from_user(currentYearGrade, "Enter student current year grade. From 1 to 100: ")
 
         if student_age > 18:
             previousYearGrade = input("Enter student previous year grade. From 1 to 100: ")
-            while not previousYearGrade.isdigit() or previousYearGrade == "0":
-                previousYearGrade = input("Enter student previous year grade. From 1 to 100: ")
-
-            previousYearGrade = int(previousYearGrade)
+            previousYearGrade = functions.input_digit_from_user(previousYearGrade,"Enter student current year grade. From 1 to 100: ")
 
             studentGrade = (previousYearGrade + currentYearGrade) / 2
+
             if studentGrade >= 50:
                 print("Congratulation. The {} {} student pass the exam".format(name, surname))
             else:
                 print("The {} {} fails. Need to hug".format(name, surname))
 
-
-            # Add to list
-            students1.update(functions.user_data(name, surname, student_age, currentYearGrade, student_counts))
-
-            student = f"Student number: {student_counts}, name: {name}, surname: {surname}, age {student_age}, grade: {currentYearGrade}"
-            students.insert(student_counts, student)
-
-
+            # Add to dict
+            students.update(functions.user_data(name, surname, student_age, studentGrade, student_counts))
 
         else:
             print(
@@ -74,10 +52,8 @@ while student_counts < max_student_number:
             else:
                 print("The {} {} fails. Need to hug".format(name, surname))
 
-            # Add to list
-            students1.update(functions.user_data(name, surname, student_age, currentYearGrade, student_counts))
-            student = f"Student number: {student_counts}, name: {name}, surname: {surname}, age {student_age}, grade: {currentYearGrade}"
-            students.insert(student_counts, student)
+            # Add to dict
+            students.update(functions.user_data(name, surname, student_age, currentYearGrade, student_counts))
 
     else:
         print("Hmmm. There is no issue, for you we have special offers. Call us")
@@ -100,8 +76,8 @@ while student_counts < max_student_number:
 
     student_counts += 1
 
-if students1:
-    print("----------------{}---------------------------".format(students1))
+if students:
+    print("----------------{}---------------------------".format(students))
 
 
 
