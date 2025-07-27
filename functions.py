5106d6ce8c6c1a47ea2376120e607edfa5d068f8
Dilanyan <dilanyan.n@gmail.com>
2025-07-27 22:26:32 +0400

    add user input file

--- functions.py
+++ functions.py
@@ -11,17 +11,34 @@ def input_username_surename(name_surname, text=''):
         name_surname = input(f"{text}: ")
     return name_surname.capitalize()
 
-# Store the user data in the dict
+# Store the user data in file/dict
 def user_data(user_name, user_surname, user_age, user_grade, user_number):
-    current_year = datetime.datetime.now().year - user_age
+    user_email = user_name + '.' + user_surname + str(user_number) + '@myschool.armstqb'
+    user_exam = "Pass" if user_grade >= 50 else "Fail"
+
+    # Put users to dict
     return {user_number:{
         'name': user_name.title(),
         'surname': user_surname.title(),
         'age': user_age,
         'grade': user_grade,
-        'email': user_name + '.' + user_surname + str(current_year) + '@myschool.armstqb',
-        'exam': "Pass" if user_grade >= 50 else "Fail"
+        'email': user_email,
+        'exam': user_exam
     }}
+
+# Store the user data in file/dict
+def input_users_into_file(user_name, user_surname, user_age, user_grade, user_number, *args):
+    user_email = user_name + '.' + user_surname + str(user_number) + '@myschool.armstqb'
+    user_exam = "Pass" if user_grade >= 50 else "Fail"
+
+    try:
+        with open("StudentsReport.txt", "a") as file:
+            file.write(f"Name: {user_name.title()}, Surname: {user_surname.title()}, Age: {user_age}, Grade: {user_grade}, Email: {user_email}, Exam: {user_exam}\n")
+    except FileNotFoundError:
+        print("The file doesn't exist.")
+    finally:
+        print("Operation complete.")
+
 
 # Neat print of student's data
 def students_print(students):
--- main.py
+++ main.py
@@ -1,84 +1,132 @@
-import functions
+from itertools import count
 
-input_max_student_number = input("Input the maximum number of students: ")
-max_student_number = functions.input_digit_from_user(input_max_student_number, "Input the maximum number of students")
+import functions
+import os
 
 students = dict()
 student_counts = 0
 input_student_or_not = ""
-input_student_or_not1 = True
 
-while student_counts < max_student_number:
+# Add a new input value that will check if students’ data will be provided manually or from a file
+while True:
+    enter_students_manually = input("Input students manually? yes/no: ")
+    if enter_students_manually.lower() == 'yes':
+        break
+    elif enter_students_manually.lower() == 'no':
+        break
+    else:
+        print('Type yes/no')
 
-    name = input("Enter student name: ")
-    username = functions.input_username_surename(name, "Enter student name")
+if enter_students_manually == "yes":
 
-    surname = input("Enter student surname: ")
-    user_surname = functions.input_username_surename(surname, "Enter student surname")
-
-    student_age = input("Enter student age (from 18 to 120): ")
-    student_age = functions.input_digit_from_user(student_age, "Enter student age (from 18 to 120)")
+    input_max_student_number = input("Input the maximum number of students: ")
+    max_student_number = functions.input_digit_from_user(input_max_student_number, "Input the maximum number of students")
 
 
-    if student_age < 18:
-        print("Dear {} {} is under 18 so, for now he/she is a primary school student".format(name, surname, student_age))
-    elif 18 <= student_age <= 120:
-        print("Dear {} {} student, is a college student".format(name, surname))
-        print("Now let's to calculate the average grade for previous and current year - ")
 
-        currentYearGrade = input("Enter student current year grade. From 1 to 100: ")
-        currentYearGrade = functions.input_digit_from_user(currentYearGrade, "Enter student current year grade. From 1 to 100: ")
+    while student_counts < max_student_number:
 
-        if student_age > 18:
-            previousYearGrade = input("Enter student previous year grade. From 1 to 100: ")
-            previousYearGrade = functions.input_digit_from_user(previousYearGrade,"Enter student current year grade. From 1 to 100: ")
+        name = input("Enter student name: ")
+        username = functions.input_username_surename(name, "Enter student name")
 
-            studentGrade = (previousYearGrade + currentYearGrade) / 2
+        surname = input("Enter student surname: ")
+        user_surname = functions.input_username_surename(surname, "Enter student surname")
 
-            if studentGrade >= 50:
-                print("Congratulation. The {} {} student pass the exam".format(name, surname))
+        student_age = input("Enter student age (from 18 to 120): ")
+        student_age = functions.input_digit_from_user(student_age, "Enter student age (from 18 to 120)")
+
+
+        if student_age < 18:
+            print("Dear {} {} is under 18 so, for now he/she is a primary school student".format(name, surname, student_age))
+        elif 18 <= student_age <= 120:
+            print("Dear {} {} student, is a college student".format(name, surname))
+            print("Now let's to calculate the average grade for previous and current year - ")
+
+            currentYearGrade = input("Enter student current year grade. From 1 to 100: ")
+            currentYearGrade = functions.input_digit_from_user(currentYearGrade, "Enter student current year grade. From 1 to 100: ")
+
+            if student_age > 18:
+                previousYearGrade = input("Enter student previous year grade. From 1 to 100: ")
+                previousYearGrade = functions.input_digit_from_user(previousYearGrade,"Enter student current year grade. From 1 to 100: ")
+
+                studentGrade = (previousYearGrade + currentYearGrade) / 2
+
+                if studentGrade >= 50:
+                    print("Congratulation. The {} {} student pass the exam".format(name, surname))
+                else:
+                    print("The {} {} fails. Need to hug".format(name, surname))
+
+                # Add to dict
+                students.update(functions.user_data(name, surname, student_age, studentGrade, student_counts))
+                functions.input_users_into_file(name, surname, student_age, studentGrade, student_counts)
+
             else:
-                print("The {} {} fails. Need to hug".format(name, surname))
+                print(
+                    "This  {} {} student is only 18 year old, obviously he hasn't previous year grade. So calculating the current year grade ".format(
+                        name, surname))
 
-            # Add to dict
-            students.update(functions.user_data(name, surname, student_age, studentGrade, student_counts))
+                if currentYearGrade >= 50:
+                    print("Congratulation. The {} {} student pass the exam".format(name, surname))
+                else:
+                    print("The {} {} fails. Need to hug".format(name, surname))
+
+                # Add to dict
+                students.update(functions.user_data(name, surname, student_age, currentYearGrade, student_counts))
+                functions.input_users_into_file(name, surname, student_age, currentYearGrade, student_counts)
 
         else:
-            print(
-                "This  {} {} student is only 18 year old, obviously he hasn't previous year grade. So calculating the current year grade ".format(
-                    name, surname))
+            print("Hmmm. There is no issue, for you we have special offers. Call us")
+            students.update(functions.user_data(name, surname, student_age, 0, student_counts))
+            functions.input_users_into_file(name, surname, student_age, 0, student_counts)
 
-            if currentYearGrade >= 50:
-                print("Congratulation. The {} {} student pass the exam".format(name, surname))
-            else:
-                print("The {} {} fails. Need to hug".format(name, surname))
+        if 18 <= student_age <= 120:
+            print(f"{student_counts + 1} student(s) entered so far.")
 
-            # Add to dict
-            students.update(functions.user_data(name, surname, student_age, currentYearGrade, student_counts))
+        if student_counts + 1 != max_student_number:
+            while student_counts < max_student_number:
+                input_student_or_not = input('Continue to input a student? yes/no: ')
+                if input_student_or_not.lower() == 'yes':
+                    break
+                elif input_student_or_not.lower() == 'no':
+                    break
+                else:
+                    print('Type yes/no')
 
-    else:
-        print("Hmmm. There is no issue, for you we have special offers. Call us")
-        students.update(functions.user_data(name, surname, student_age, 0, student_counts))
+        if input_student_or_not == 'no':
+            break
 
-    if 18 <= student_age <= 120:
-        print(f"{student_counts + 1} student(s) entered so far.")
+        student_counts += 1
 
-    if student_counts + 1 != max_student_number:
-        while student_counts < max_student_number:
-            input_student_or_not = input('Continue to input a student? yes/no: ')
-            if input_student_or_not.lower() == 'yes':
-                break
-            elif input_student_or_not.lower() == 'no':
-                break
-            else:
-                print('Type yes/no')
+    functions.students_print(students)
 
-    if input_student_or_not == 'no':
-        break
 
-    student_counts += 1
 
-functions.students_print(students)
+
+else:
+    # Input students form StudentsList.txt file to StudentsReport.txt file
+    file_path_name = input("Input the “StudentsList.txt” file path (Like C:\\Users\\'username'\\Downloads or C:\\Users\\username\\Documents): ")
+    os.chdir(file_path_name)
+    try:
+        with open("StudentsList.txt", "r") as file:
+            content = file.readlines()
+            for user_row in content:
+                one_student_info = user_row.strip().split(' ')
+                studentGrade = (int(one_student_info[3]) + int(one_student_info[4])) / 2
+                students.update(functions.user_data(one_student_info[0], one_student_info[1], one_student_info[2], studentGrade, student_counts))
+                student_counts += 1
+
+    except FileNotFoundError:
+        print("The file doesn't exist.")
+    finally:
+        print("Operation complete.")
+
+    for student_id, student_value in students.items():
+        print(student_value['name'], student_value['surname'], student_value['age'], student_value['grade'], student_value['email'], student_value['exam'])
+        functions.input_users_into_file(student_value['name'], student_value['surname'], student_value['age'], student_value['grade'], student_value['email'], student_value['exam'])
+
+
+
+
 
 
 
