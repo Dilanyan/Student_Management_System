d0fb4cd78f02b5fce0b795055db7e671f74314e1
Dilanyan <dilanyan.n@gmail.com>
2025-09-23 21:11:40 +0400

    gorcnakanan selenium 2

---
+++ s_locator_gorcnakan.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+try:
+    driver.get("https://www.saucedemo.com/")
+    driver.maximize_window()
+    element1 = driver.find_element(By.ID, "user-name")
+    element1.send_keys("standard_user")
+    element2 = driver.find_element(By.ID,"password")
+    element2.send_keys("secret_sauce")
+    element3 = driver.find_element(By.NAME, "login-button")
+    element3.click()
+    element4 = driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+    assert element4.is_displayed()
+
+
+finally:
+    driver.quit()
